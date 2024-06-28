d0abbc04cdbc14d97e215e5b8df8249d47f23f2a
Vijay Jagtap <104545521+VijayJagtap1@users.noreply.github.com>
2024-06-28 19:49:34 +0530

    Added CRUD operations
    All the required crud operations are added to the services

--- atm-simulator/src/main/java/com/atm/services/AtmService.java
+++ atm-simulator/src/main/java/com/atm/services/AtmService.java
@@ -1,4 +1,4 @@
-package com.atm.services;
+package com.atm.service;
 
 import java.util.List;
 
@@ -7,25 +7,101 @@ import org.springframework.stereotype.Service;
 
 import com.atm.entities.Atm;
 import com.atm.repositorie.AtmRepository;
+import com.atm.serviceinterface.AtmServiceInterface;
 
 @Service
-public class AtmService {
+public class AtmService implements AtmServiceInterface
+{
 	@Autowired
-	private AtmRepository Atmrepo ;
+	private AtmRepository atmRepoRef;
 	
-	public void insertAtmDetails(Atm a1) {
-		Atmrepo.save(a1);
+	//insert one Atm details
+	public void insertAtmDetails(Atm atmDetail)
+	{
+		atmRepoRef.save(atmDetail);
 	}
-	 public List<Atm> showAllAtm(){
-		 List<Atm>Atmlist = Atmrepo.findAll();
-		 return Atmlist;
-	 }
-//	 public Atm getSingleAtm(int id) {
-//		 Atm a1 = null;
-//		List<Atm> Atmlist =Atmrepo.findAll();
-//		for(Atm a :Atmlist) {
-//			if(a.get == id) {
-//		}
-//	 }
-	 
+	
+	//insert Array of Atm details
+	public void insertArrayOfAtmDetails(List<Atm> atmDetail)
+	{
+		atmRepoRef.saveAll(atmDetail);
+	}
+	
+	//Show All atm
+	public List<Atm> atmDetails()
+	{
+		return atmRepoRef.findAll();
+	}
+		
+	//show atm by id
+	public Atm findAtmById(int id)
+	{
+		List<Atm> ls = atmRepoRef.findAll();
+		Atm foundAtm = null;
+		for(Atm a : ls)
+		{
+			if(id==a.getId())
+			{
+				foundAtm = a;
+				break;
+			}
+		}
+		return foundAtm;
+	}
+	
+	//show atm by name
+	public Atm findAtmByName(String name)
+	{
+		List<Atm> ls = atmRepoRef.findAll();
+		Atm foundAtm = null;
+		for(Atm a : ls)
+		{
+			if(name==a.getName())
+			{
+				foundAtm = a;
+				break;
+			}
+		}
+		return foundAtm;
+	}
+	
+	public void deleteAtmById(int id)
+	{
+		List<Atm> ls = atmRepoRef.findAll();
+		for(Atm a : ls)
+		{
+			if(a.getId() == id)
+			{
+				atmRepoRef.delete(a);
+			}
+		}
+	}
+	
+	public void updateAtm(Atm updateAtm)
+	{
+		List<Atm> ls = atmRepoRef.findAll();
+		for(Atm a : ls)
+		{
+			if(updateAtm.getId() == a.getId())
+			{
+				atmRepoRef.save(a);
+				
+			}
+		}
+	}
+	
+	public void updateArrayAtm(List<Atm> listAtmDetails)
+	{
+		List<Atm> ls = atmRepoRef.findAll();
+		for(Atm a : ls)
+		{
+		for(Atm b : listAtmDetails)
+		{
+			if(a.getId() == b.getId())
+			{
+				atmRepoRef.save(b);
+			}
+		}
+	}
+	}
 }
